463ce76903e89d868107f6f86ac5d2440c5882ee
Sourabhkaushish <00sourabh52@gmail.com>
2026-07-14 11:17:57 +0530

    first commit

--- CampusBoard/src/App.jsx
+++ CampusBoard/src/App.jsx
@@ -1,62 +1,25 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import ProtectedRoute from "./routes/ProtectedRoute";
+// 
+<Routes>
+  <Route path="/" element={<Login />} />
+  <Route path="/signup" element={<Signup />} />
 
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
-import Profile from "./pages/Profile";
+  <Route
+    path="/dashboard"
+    element={
+      <ProtectedRoute>
+        <Dashboard />
+      </ProtectedRoute>
+    }
+  />
 
-export default function App() {
-  return (
-    <AuthProvider>
-      <Routes>
+  <Route
+    path="/profile"
+    element={
+      <ProtectedRoute>
+        <Profile />
+      </ProtectedRoute>
+    }
+  />
 
-        {/* Redirect root to dashboard */}
-        <Route 
-          path="/" 
-          element={<Navigate to="/dashboard" replace />} 
-        />
-
-        {/* Public Routes */}
-        <Route 
-          path="/login" 
-          element={<Login />} 
-        />
-
-        <Route 
-          path="/signup" 
-          element={<Signup />} 
-        />
-
-
-        {/* Protected Routes */}
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-
-
-        {/* Handle unknown routes */}
-        <Route
-          path="*"
-          element={<Navigate to="/login" replace />}
-        />
-
-      </Routes>
-    </AuthProvider>
-  );
-}
+  <Route path="*" element={<Navigate to="/" replace />} />
+</Routes>
